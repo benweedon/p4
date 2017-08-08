babe88ff0c7d767e934a0be57ad6a1bb3ec48678
Ben Weedon <ben.weedon@outlook.com>
2017-08-07 17:46:35 -0700

    Support use of p4 functions
    This change is quite large. The source files provided by the user are
    copied to a temp directory. Those files are then preprocessed to remove
    any //## comments. Also, p4 functions are now supported and p4Save is
    added.

--- p4.py
+++ p4.py
@@ -1,5 +1,12 @@
 import argparse
+import distutils.dir_util
+import os
+import re
 import subprocess
+import tempfile
+
+P4_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.join(P4_DIR, "lib")
 
 def main():
     # parse arguments
@@ -7,7 +14,42 @@ def main():
     parser.add_argument('source_dir', metavar='source_dir', type=str, help='the directory of the source files')
     args = parser.parse_args()
 
-    subprocess.call(["processing-java", "--sketch=" + args.source_dir, "--run"])
+    with tempfile.TemporaryDirectory() as temp_dir:
+        package_dir = copy_source(args.source_dir, temp_dir)
+        preprocess_source(package_dir)
+        subprocess.call(["processing-java", "--sketch=" + package_dir, "--run"])
+
+
+def copy_source(src, dest):
+    # copy the user-produced source
+    package_name = os.path.basename(os.path.normpath(os.path.abspath(src)))
+    package_dir = os.path.join(dest, package_name)
+    distutils.dir_util.copy_tree(src, package_dir)
+
+    # copy the p4 libraries
+    distutils.dir_util.copy_tree(LIB_DIR, package_dir)
+
+    return package_dir
+
+
+def preprocess_source(source_dir):
+    for filename in os.listdir(source_dir):
+        if filename.endswith(".pde"):
+            with open(os.path.join(source_dir, filename), 'r+') as f:
+                new_lines = []
+                for line in f:
+                    new_line = line
+                    new_line = preprocess_hash_comments(new_line)
+                    new_lines.append(new_line)
+                f.seek(0)
+                for line in new_lines:
+                    f.write(line)
+                f.truncate()
+
+
+def preprocess_hash_comments(line):
+    return re.sub('//##', '', line)
+
 
 if __name__ == '__main__':
     main()
