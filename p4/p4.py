8b1946f5ba2aed3b54357a9abe91fc764910b474
Ben Weedon <ben.weedon@outlook.com>
2017-08-16 11:18:54 -0700

    Support a passthru parameter and add p4Globals.pde
    If passthru is specified, the save file path will be written to stdout.
    This change also removes the "Finished." string which Processing sends
    to stdout on success, and introduces the p4Globals.pde file, which
    contains any global variables accessible to Processing code run through
    p4.

--- p4/p4.py
+++ p4/p4.py
@@ -11,12 +11,19 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser(description='Preprocess a Processing source file.')
     parser.add_argument('source_dir', metavar='source_dir', type=str, help='the directory of the source files')
+    parser.add_argument('--passthru', '-p', dest='passthru', action='store_true', help='write the save file path to stdout')
     args = parser.parse_args()
 
     with tempfile.TemporaryDirectory() as temp_dir:
         package_dir = copy_source(args.source_dir, temp_dir)
+        create_globals_file(package_dir, args)
         preprocess_source(package_dir)
-        subprocess.call(["processing-java", "--sketch=" + package_dir, "--run"])
+        result = subprocess.run(['processing-java', '--sketch=' + package_dir, '--run'], stdout=subprocess.PIPE, encoding='utf8')
+        lines = result.stdout.splitlines()
+        if len(lines) > 0 and lines[-1] == 'Finished.':
+            lines = lines[:-1]
+        for line in lines:
+            print(line)
 
 
 def copy_source(src, dest):
@@ -31,9 +38,14 @@ def copy_source(src, dest):
     return package_dir
 
 
+def create_globals_file(package_dir, args):
+    with open(os.path.join(package_dir, 'p4Globals.pde'), 'w') as f:
+        print(f'static final boolean PASSTHRU = {str(args.passthru).lower()};', file=f)
+
+
 def preprocess_source(source_dir):
     for filename in os.listdir(source_dir):
-        if filename.endswith(".pde"):
+        if filename.endswith('.pde'):
             with open(os.path.join(source_dir, filename), 'r+') as f:
                 new_lines = []
                 for line in f:
